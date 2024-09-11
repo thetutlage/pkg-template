24f68ce898338b346e38e83d0bef857a446cf0ac
Harminder Virk <virk.officials@gmail.com>
2024-09-11 08:57:57 +0530

    feat: setup japa

---
+++ bin/test.ts
@@ -0,0 +1,12 @@
+import { expect } from '@japa/expect'
+import { fileSystem } from '@japa/file-system'
+import { expectTypeOf } from '@japa/expect-type'
+import { configure, processCLIArgs, run } from '@japa/runner'
+
+processCLIArgs(process.argv.splice(2))
+configure({
+  files: ['tests/**/*.spec.ts'],
+  plugins: [expect(), fileSystem(), expectTypeOf()],
+})
+
+run()
---
+++ tests/maths.spec.ts
@@ -0,0 +1,7 @@
+import { test } from '@japa/runner'
+test.group('Maths.add', () => {
+  test('add two numbers', ({ expect }) => {
+    // Test logic goes here
+    expect(1 + 1).toBe(2)
+  })
+})
