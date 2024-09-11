8234cd8eabfa14dd1af79dd6c49a642cde28bd3a
Harminder Virk <virk.officials@gmail.com>
2024-09-11 09:03:25 +0530

    test: fix breaking test

--- bin/test.ts
+++ bin/test.ts
@@ -1,4 +1,5 @@
 import { expect } from '@japa/expect'
+import { assert } from '@japa/assert'
 import { fileSystem } from '@japa/file-system'
 import { expectTypeOf } from '@japa/expect-type'
 import { configure, processCLIArgs, run } from '@japa/runner'
@@ -6,7 +7,7 @@ import { configure, processCLIArgs, run } from '@japa/runner'
 processCLIArgs(process.argv.splice(2))
 configure({
   files: ['tests/**/*.spec.ts'],
-  plugins: [expect(), fileSystem(), expectTypeOf()],
+  plugins: [expect(), assert(), fileSystem(), expectTypeOf()],
 })
 
 run()
--- tests/maths.spec.ts
+++ tests/maths.spec.ts
@@ -1,4 +1,5 @@
 import { test } from '@japa/runner'
+
 test.group('Maths.add', () => {
   test('add two numbers', ({ expect }) => {
     // Test logic goes here
